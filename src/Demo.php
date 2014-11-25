11d901561ac0c122e3eaf9d841b828a35e9cd329
nativeby <nativeby@163.com>
2014-11-25 09:49:30 +0800

    update demo

--- src/Demo.php
+++ src/Demo.php
@@ -1,19 +1,17 @@
 <?php
 
-    use Validator\CustomRules;
+    require 'vendor/autoload.php';
 
-    require_once('Validator/CustomRules.php');
+    $data = array(
+        'mobile' => '1234578965',
+        'email' => 'aaatestbaihe.com',
+    );
+
+    $ret = Validator\CustomRules::validator($data);
 
     try{
-
-        $data = array(
-            'mobile' => '1234578965',
-            'email' => 'aaatestnext.com',
-        );
-
-        $ret = CustomRules::validator($data);
-
         if($ret !== true){
+            var_dump($ret);
             throw new Exception('验证不通过',-1);
         }
     }catch(Exception $e) {
